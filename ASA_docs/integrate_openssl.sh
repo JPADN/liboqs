9b5ea569b73af4b251abfd1c51ee928a3897859b
JPADN <jpadnalemao@gmail.com>
2021-09-10 11:21:43 -0300

    Change openssl branch

--- ASA_docs/integrate_openssl.sh
+++ ASA_docs/integrate_openssl.sh
@@ -12,7 +12,7 @@ read OPENSSL_DIR
 
 sudo apt install cmake gcc libtool libssl-dev make ninja-build git
 
-git clone --single-branch --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/openssl.git ${OPENSSL_DIR}
+git clone --single-branch --branch OQS-OpenSSL_1_1_1-stable-snapshot-2021-03 https://github.com/open-quantum-safe/openssl.git ${OPENSSL_DIR}
 
 cd ${OPENSSL_DIR}
 ./Configure no-shared linux-x86_64 -lm
